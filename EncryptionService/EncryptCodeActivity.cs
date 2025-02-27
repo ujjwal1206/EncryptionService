4174b994d87dd97f90103aa11b0aa2fa04a21a6e
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2025-02-27 21:46:29 +0530

    Add project files.

---
+++ EncryptionService/EncryptCodeActivity.cs
@@ -0,0 +1,21 @@
+﻿using System.Activities;
+
+public sealed class EncryptCodeActivity : CodeActivity<string>
+{
+    public InArgument<string> PlainText { get; set; }
+    public InArgument<int> Shift { get; set; }
+
+    protected override string Execute(CodeActivityContext context)
+    {
+        string text = PlainText.Get(context);
+        int shift = Shift.Get(context);
+        char[] buffer = text.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+            letter = (char)(letter + shift);
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+}
